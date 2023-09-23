5f95a323246ef30320c80160d3f258a0b299e9b8
aangelone2 <adriano.angelone.work@gmail.com>
2023-09-23 12:28:47 +0200

    refined testing, tests pass, added poetry support

--- tests/test_parse_ds.py
+++ tests/test_parse_ds.py
@@ -18,7 +18,9 @@ def test_empty_lines():
     """Testing parsing of file with empty lines."""
 
     ds = parse_ds("tests/data/empty_lines.dat", None, True)
-    assert ds.size == np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+    assert np.array_equal(
+        ds, np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+    )
 
 
 def test_empty_column():
@@ -35,10 +37,12 @@ def test_empty_column():
         )
 
     # only parsing cols [0,1], testing colnum
-    ds = parse_ds(
-        "tests/data/empty_column.dat", [0, 1], colnum_test=True
-    )
-    assert ds.size == np.array([[1, 2], [5, 6], [1, 2]])
+    with pytest.raises(ValueError):
+        ds = parse_ds(
+            "tests/data/empty_column.dat",
+            [0, 1],
+            colnum_test=True,
+        )
 
     # parsing columns [2,4], testing colnum
     with pytest.raises(ValueError):
@@ -57,12 +61,15 @@ def test_empty_column():
         )
 
     # only parsing cols [0,1], NOT testing colnum
+    # should pass (missing column not within parsed span)
     ds = parse_ds(
         "tests/data/empty_column.dat",
         [0, 1],
         colnum_test=False,
     )
-    assert ds.size == np.array([[1, 2], [5, 6], [1, 2]])
+    assert np.array_equal(
+        ds, np.array([[1, 2], [5, 6], [1, 2]])
+    )
 
     # parsing columns [2,4], NOT testing colnum
     with pytest.raises(ValueError):
