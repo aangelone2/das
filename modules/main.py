b365a3b56467f945acb9945ea7d4c14bf2539fe1
aangelone2 <adriano.angelone.work@gmail.com>
2023-09-26 20:13:53 +0200

    organized output for avs, added rich

--- modules/main.py
+++ modules/main.py
@@ -2,9 +2,14 @@
 
 
 import argparse
+from rich.console import Console
+from rich.table import Table
 
-from common import parse_ds
-from avs import avs
+from modules.common import parse_ds
+from modules.drivers import avs
+
+
+console = Console()
 
 
 def main():
@@ -15,7 +20,8 @@ def main():
     parser.add_argument(
         "-f",
         "--fields",
-        help="comma-separated columns to average (default = all)",
+        type=str,
+        help="comma-separated, 1-indexed fields to analyze (default = all)",
         default=None,
     )
     parser.add_argument(
@@ -38,12 +44,6 @@ def main():
         description="performs averaging without rebinning",
     )
     avs_parser.add_argument(
-        "-c",
-        "--compatibility",
-        help="use a power of 2 of points for compatibility with ave",
-        action="store_true",
-    )
-    avs_parser.add_argument(
         "-s",
         "--skip",
         help="percentage (1-100) of rows to skip (default = 0)",
@@ -53,11 +53,40 @@ def main():
 
     args = parser.parse_args()
 
+    # converting to list of integers,
+    # raises ValueError and terminates if invalid value
+    if args.fields is not None:
+        args.fields = [
+            (int(s) - 1) for s in args.fields.split(",")
+        ]
+
     # parsing (common)
     ds = parse_ds(args.file, args.fields, not args.quick)
 
     if args.command == "avs":
-        avs(ds, args.skip, args.compatibility, args.verbose)
+        stats, _ = avs(ds, args.skip)
+
+        table = Table(box=None)
+        table.add_column("column")
+        table.add_column("mean")
+        table.add_column("σ of mean")
+        table.add_column("σ of σ of mean")
+
+        cols = (
+            range(len(stats))
+            if args.fields is None
+            else args.fields
+        )
+
+        for col, col_stats in zip(cols, stats):
+            table.add_row(
+                f"{col}",
+                f'{col_stats["m"]:.11e}',
+                f'{col_stats["s"]:.1e}',
+                f'{col_stats["ds"]:.1e}',
+            )
+
+        console.print(table)
 
 
 if __name__ == "__main__":
