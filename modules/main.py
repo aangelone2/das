2ea9f64868d70f1b95946fdf1d708f302b17a830
aangelone2 <adriano.angelone.work@gmail.com>
2023-09-26 21:08:46 +0200

    reorganized main parser to recycle code and have right option/command order

--- modules/main.py
+++ modules/main.py
@@ -14,41 +14,44 @@ console = Console()
 
 def main():
     """Implement main entrypoint."""
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("file", help="file to analyze")
-    parser.add_argument(
+    # parent parser with shared options
+    parent_parser = argparse.ArgumentParser(add_help=False)
+    parent_parser.add_argument(
         "-f",
         "--fields",
         type=str,
         help="comma-separated, 1-indexed fields to analyze (default = all)",
         default=None,
     )
-    parser.add_argument(
+    parent_parser.add_argument(
         "-q",
         "--quick",
         help="skip row integrity check",
         action="store_true",
     )
-    parser.add_argument(
+    parent_parser.add_argument(
         "-v",
         "--verbose",
         help="verbose output",
         action="store_true",
     )
-
-    subp = parser.add_subparsers(dest="command")
-
-    avs_parser = subp.add_parser(
-        "avs",
-        description="performs averaging without rebinning",
-    )
-    avs_parser.add_argument(
+    parent_parser.add_argument(
         "-s",
         "--skip",
         help="percentage (1-100) of rows to skip (default = 0)",
         type=int,
         default=0,
+    )
+    parent_parser.add_argument("file", help="file to analyze")
+
+    # main parser
+    parser = argparse.ArgumentParser()
+    subp = parser.add_subparsers(dest="command")
+
+    _ = subp.add_parser(
+        "avs",
+        description="performs averaging without rebinning",
+        parents=[parent_parser],
     )
 
     args = parser.parse_args()
